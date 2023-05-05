77f919a5249dd58417dbb74b223cea58e8605ca4
borgesdan <borges_santos89@hotmail.com>
2023-05-05 10:29:57 -0300

    Implementa GamePadButtons

--- src/input/gamepad.hpp
+++ src/input/gamepad.hpp
@@ -1,6 +1,8 @@
 #ifndef XNA_INPUT_GAMEPAD_HPP
 #define XNA_INPUT_GAMEPAD_HPP
 
+#include <string>
+#include <vector>
 #include "enumerations.hpp"
 #include "../mathhelper.hpp"
 #include "../basic-structs.hpp"
@@ -33,11 +35,11 @@ namespace xna {
 	struct GamePadThumbSticks {
 		constexpr GamePadThumbSticks() = default;
 
-		GamePadThumbSticks(Vec2 const& leftPosition, Vec2 const& rightPosition,
+		constexpr GamePadThumbSticks(Vec2 const& leftPosition, Vec2 const& rightPosition,
 			GamePadDeadZone leftDeadZoneMode = GamePadDeadZone::None, GamePadDeadZone rightDeadZoneMode = GamePadDeadZone::None) {
 
 			_left = ApplyDeadZone(leftDeadZoneMode, leftThumbDeadZone, leftPosition);
-			_right = ApplyDeadZone(rightDeadZoneMode, rightThumbDeadZone, rightPosition);			
+			_right = ApplyDeadZone(rightDeadZoneMode, rightThumbDeadZone, rightPosition);
 
 			if (leftPosition.X < -leftThumbDeadZone)
 				_virtualButtons = static_cast<Buttons>(toint(_virtualButtons) | toint(Buttons::LeftThumbstickLeft));
@@ -84,12 +86,12 @@ namespace xna {
 		}
 
 	private:
-		static constexpr double leftThumbDeadZone{ 0 };
-		static constexpr double rightThumbDeadZone{ 0 };
+		static constexpr double leftThumbDeadZone{ 0 }; //TODO
+		static constexpr double rightThumbDeadZone{ 0 }; //TODO
 		Vec2 _left;
 		Vec2 _right;
 
-		Vec2 ApplyDeadZone(GamePadDeadZone deadZoneMode, double deadZone, Vec2 thumbstickPosition) {			
+		constexpr Vec2 ApplyDeadZone(GamePadDeadZone deadZoneMode, double deadZone, Vec2 thumbstickPosition) {
 
 			switch (deadZoneMode)
 			{
@@ -114,11 +116,11 @@ namespace xna {
 			return thumbstickPosition;
 		}
 
-		Vec2 ExcludeIndependentAxesDeadZone(Vec2 const& value, double deadZone) const {
+		constexpr Vec2 ExcludeIndependentAxesDeadZone(Vec2 const& value, double deadZone) const {
 			return Vec2(ExcludeAxisDeadZone(value.X, deadZone), ExcludeAxisDeadZone(value.Y, deadZone));
 		}
 
-		double ExcludeAxisDeadZone(double value, double deadZone) const {
+		constexpr double ExcludeAxisDeadZone(double value, double deadZone) const {
 			if (value < -deadZone)
 				value += deadZone;
 			else if (value > deadZone)
@@ -129,7 +131,7 @@ namespace xna {
 			return value / (1.0 - deadZone);
 		}
 
-		Vec2 ExcludeCircularDeadZone(Vec2 const& value, float deadZone) const {
+		constexpr Vec2 ExcludeCircularDeadZone(Vec2 const& value, float deadZone) const {
 			const auto originalLength = value.Length();
 
 			if (originalLength <= deadZone)
@@ -138,6 +140,156 @@ namespace xna {
 			const auto newLength = (originalLength - deadZone) / (1.0 - deadZone);
 
 			return value * (newLength / originalLength);
+		}
+	};
+
+	struct GamePadCapabilities {
+		bool IsConnected{ false };
+		bool HasAButton{ false };
+		bool HasBackButton{ false };
+		bool HasBButton{ false };
+		bool HasDPadDownButton{ false };
+		bool HasDPadLeftButton{ false };
+		bool HasDPadRightButton{ false };
+		bool HasDPadUpButton{ false };
+		bool HasLeftShoulderButton{ false };
+		bool HasLeftStickButton{ false };
+		bool HasRightShoulderButton{ false };
+		bool HasRightStickButton{ false };
+		bool HasStartButton{ false };
+		bool HasXButton{ false };
+		bool HasYButton{ false };
+		bool HasBigButton{ false };
+		bool HasLeftXThumbStick{ false };
+		bool HasLeftYThumbStick{ false };
+		bool HasRightXThumbStick{ false };
+		bool HasRightYThumbStick{ false };
+		bool HasLeftTrigger{ false };
+		bool HasRightTrigger{ false };
+		bool HasLeftVibrationMotor{ false };
+		bool HasRightVibrationMotor{ false };
+		bool HasVoiceSupport{ false };
+		std::string DisplayName;
+		std::string Identifier;
+		GamePadType GamePadType;
+
+		constexpr bool Equals(GamePadCapabilities const& other) const {
+			return
+			(DisplayName == other.DisplayName);
+			(Identifier == other.Identifier);
+			(IsConnected == other.IsConnected);
+			(HasAButton == other.HasAButton);
+			(HasBackButton == other.HasBackButton);
+			(HasBButton == other.HasBButton);
+			(HasDPadDownButton == other.HasDPadDownButton);
+			(HasDPadLeftButton == other.HasDPadLeftButton);
+			(HasDPadRightButton == other.HasDPadRightButton);
+			(HasDPadUpButton == other.HasDPadUpButton);
+			(HasLeftShoulderButton == other.HasLeftShoulderButton);
+			(HasLeftStickButton == other.HasLeftStickButton);
+			(HasRightShoulderButton == other.HasRightShoulderButton);
+			(HasRightStickButton == other.HasRightStickButton);
+			(HasStartButton == other.HasStartButton);
+			(HasXButton == other.HasXButton);
+			(HasYButton == other.HasYButton);
+			(HasBigButton == other.HasBigButton);
+			(HasLeftXThumbStick == other.HasLeftXThumbStick);
+			(HasLeftYThumbStick == other.HasLeftYThumbStick);
+			(HasRightXThumbStick == other.HasRightXThumbStick);
+			(HasRightYThumbStick == other.HasRightYThumbStick);
+			(HasLeftTrigger == other.HasLeftTrigger);
+			(HasRightTrigger == other.HasRightTrigger);
+			(HasLeftVibrationMotor == other.HasLeftVibrationMotor);
+			(HasRightVibrationMotor == other.HasRightVibrationMotor);
+			(HasVoiceSupport == other.HasVoiceSupport);
+			(GamePadType == other.GamePadType);
+		}
+
+		friend constexpr bool operator==(GamePadCapabilities const& right, GamePadCapabilities const& left) {
+			return left.Equals(right);
+		}
+
+		friend constexpr bool operator!=(GamePadCapabilities const& right, GamePadCapabilities const& left) {
+			return !left.Equals(right);
+		}
+	};
+
+	struct GamePadButtons {
+		Buttons _buttons;
+
+		constexpr GamePadButtons() = default;
+
+		constexpr GamePadButtons(Buttons buttons) : _buttons(buttons) {
+		}
+
+		constexpr GamePadButtons(std::vector<Buttons> buttons) {
+			for (size_t i = 0; i < buttons.size(); ++i) {
+				const auto& b = buttons[i];
+				_buttons = static_cast<Buttons>(toint(_buttons) | toint(b));
+			}
+		}
+
+		constexpr ButtonState A() {
+			return Get(Buttons::A);
+		}
+
+		constexpr ButtonState B() {
+			return Get(Buttons::B);
+		}
+
+		constexpr ButtonState Back() {
+			return Get(Buttons::Back);
+		}
+
+		constexpr ButtonState X() {
+			return Get(Buttons::X);
+		}
+
+		constexpr ButtonState Y() {
+			return Get(Buttons::Y);
+		}
+
+		constexpr ButtonState Start() {
+			return Get(Buttons::Start);
+		}
+
+		constexpr ButtonState LeftShoulder() {
+			return Get(Buttons::LeftShoulder);
+		}
+
+		constexpr ButtonState LeftStick() {
+			return Get(Buttons::LeftStick);
+		}
+		
+		constexpr ButtonState RightShoulder() {
+			return Get(Buttons::RightShoulder);
+		}
+		
+		constexpr ButtonState RightStick() {
+			return Get(Buttons::RightStick);
+		}
+		
+		constexpr ButtonState BigButton() {
+			return Get(Buttons::BigButton);
+		}
+
+		constexpr bool Equals(GamePadButtons const& other) const {
+			return _buttons == other._buttons;
+		}
+
+		friend constexpr bool operator==(GamePadButtons const& right, GamePadButtons const& left) {
+			return left.Equals(right);
+		}
+
+		friend constexpr bool operator!=(GamePadButtons const& right, GamePadButtons const& left) {
+			return !left.Equals(right);
+		}
+
+	private:
+		constexpr ButtonState Get(Buttons button) {
+			return ((static_cast<Buttons>(toint(_buttons) & toint(button))) == button)
+				? ButtonState::Pressed
+				: ButtonState::Released;
 		}
 	};
 }
